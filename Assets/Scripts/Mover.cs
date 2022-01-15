e7d1b24503d4f46b5c67706085bf107ff72f4d88
Jagdeep <jagdeep.saredia@cadju.com>
2022-01-15 17:27:47 +0000

    Initial Commit
    Creating the plane, player character and pendulum object along with behaviours.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    float xTransform = 0.01f;
+    [SerializeField]
+    float yTransform = 0f;
+    [SerializeField]
+    float zTransform = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Go();
+    }
+
+    void Go()
+    {
+        // Access transform of GameObject this script is attached to
+        transform.Translate(xTransform, yTransform, zTransform);
+    }
+}
---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0.01f;
+
+    float phase = 0;
+    float phaseDirection = 1;
+    Vector3 pointA = new Vector3(14f, 0.57f, 0f);
+    Vector3 pointB = new Vector3(-14f, 0.57f, 0f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(pointA, pointB, phase);
+        phase += Time.deltaTime * speed * phaseDirection;
+
+        if (phase >= 1 || phase <= 0)
+            phaseDirection *= -1;
+    }
+}
